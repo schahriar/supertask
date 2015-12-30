673278e6c29ac86501601c43b13f4e511ea4381b
Schahriar SaffarShargh <info@schahriar.com>
2015-12-29 17:25:38 -0800

    Removed HTML coverage report

--- coverage.js
+++ coverage.js
@@ -10,5 +10,3 @@ require('blanket')({
 
 // Run Main
 require('./tests/main.js');
-
-//console.log("<small style='opacity: 0.7; display: block; margin: 7px 13px;'> Generated on: ", moment().format('MMMM Do YYYY, h:mm:ss a'), '</small>');
